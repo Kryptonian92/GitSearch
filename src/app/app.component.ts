7f670b39d856212c77aaf0540eddb3469d6e2016
Kryptonian92 <ausar_mcgruder@yahoo.com>
2019-02-11 08:22:06 -0500

    updated design. Almost finalized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,5 @@ export class AppComponent {
   constructor(private http: HttpClient){
   }
   ngOnInit(){
-    // let obs = this.http.get('https://api.github.com/users/kryptonian92')
-    // obs.subscribe((response) => console.log(response));
   }
 }
